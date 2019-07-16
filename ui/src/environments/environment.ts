4ae9f959655636b6bf4d12b86566b1961dbac5ad
valdis.abrams <valdis.abrams@x-infotech.com>
2019-07-16 13:42:53 +0300

    Environment variables added

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  server: {
+    protocol: 'http://',
+    host: '',
+    port: ':3334'
+  }
 };
 
 /*
